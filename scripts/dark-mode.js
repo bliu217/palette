709ee211436fe2862400f97aeccdc6fc889bda94
bernard <bernardliu217@gmail.com>
2024-05-17 16:33:07 -0700

    Palette now saves the GUI theme upon exit.

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -2,34 +2,43 @@ const darkModeSwitch = document.getElementById('dark-mode-switch');
 const logoEl = document.getElementById('logo-el');
 const tools = document.querySelectorAll('.dm-tool');
 const toolContainer = document.querySelector('.tool-container');
-let darkMode = false;
+let darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
 
 export function enableDarkMode() {
+    console.log(darkMode);
+    darkModeSwitch.checked = darkMode;
+
+    switchMode();
     darkModeSwitch.addEventListener('change', () => {
         darkMode = !darkMode;
-        if (darkMode) {
-            document.body.style.backgroundColor = "#3B3B3B";
-            document.body.style.color = "#DFDFDF";
-            document.getElementById('github-link').style.color = "#DFDFDF";
-            document.querySelector('.header-el').style.backgroundColor = "#2B2B2B";
-            logoEl.innerHTML = `
-            <img src="assets/dark-full.png">
-            `;
-            changeToolsDark();
-            changeScroll();
-        } else {
-            document.body.style.backgroundColor = "white";
-            document.body.style.color = "black";
-            document.getElementById('github-link').style.color = "#464646";
-            document.querySelector('.header-el').style.backgroundColor = "white";
-            logoEl.innerHTML = `
-            <img src="assets/logo-full.png">
-            `;
-            changeToolsLight();
-            changeScroll();
-
-        }
+        localStorage.setItem('darkMode', JSON.stringify(darkMode));
+        switchMode();
     });
+}
+
+function switchMode() {
+    if (darkMode) {
+        document.body.style.backgroundColor = "#3B3B3B";
+        document.body.style.color = "#DFDFDF";
+        document.getElementById('github-link').style.color = "#DFDFDF";
+        document.querySelector('.header-el').style.backgroundColor = "#2B2B2B";
+        logoEl.innerHTML = `
+        <img src="assets/dark-full.png">
+        `;
+        changeToolsDark();
+        changeScroll();
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.getElementById('github-link').style.color = "#464646";
+        document.querySelector('.header-el').style.backgroundColor = "white";
+        logoEl.innerHTML = `
+        <img src="assets/logo-full.png">
+        `;
+        changeToolsLight();
+        changeScroll();
+
+    }
 }
 
 function changeToolsDark() {
@@ -54,6 +63,10 @@ function changeScroll() {
     const collection = document.getElementById('palette-collection')
     let isScrollbar = window.scrollbars.visible;
     if (isScrollbar) {
-        collection.classList.toggle("scrollbar-dark");
+        if (darkMode) {
+            collection.classList.add("scrollbar-dark");
+        } else {
+            collection.classList.remove("scrollbar-dark");
+        }
     }
 }
